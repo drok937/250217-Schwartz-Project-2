a1973d596b13a3af66ab0f084cd47bf6193fc9a6
drok937 <derek.schwartz1993@gmail.com>
2025-02-18 11:15:40 -0500

    Take 2
    Started to work on the JS but not working yet

--- MusicTools.js
+++ MusicTools.js
@@ -1,22 +1,22 @@
 const MusicTools = {
     midiPitchToFrequency(midiPitch) {
-        let freq = 440 * Math.pow(2, ((midiPitch - 69) / 12));
-        return freq
+        const freq = 440 * Math.pow(2, ((midiPitch - 69) / 12));
+        return freq;
     },
 
     frequencyToMidiPitch(frequency) {
-       let midiPitch =  69 + 12 * Math.log2(frequency / 440);
-        return midiPitch
+       const midiPitch =  69 + 12 * Math.log2(frequency / 440);
+        return midiPitch;
     },
     dbfsToLinearAmplitude(dbfs) {
-        let linear = Math.pow(10, dbfs / 20);
-        return linear
+        const linear = Math.pow(10, dbfs / 20);
+        return linear;
     },
 
     linearAmplitudeTodBFS(linear) {
-        let dBFS = 20 * Math.log10(linear);
-        return dBFS
+        const dBFS = 20 * Math.log10(linear);
+        return dBFS;
     },
 };
 midiPitchToFrequency(400);
-export { freq, midiPitch, linear, dBFS} ;
+export {freq, midiPitch, linear, dBFS};
--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 import {freq, midiPitch, linear, dBFS} from "./MusicTools.js";
 
+let midiInput = document.getElementById("midiToFreq");
+let freqInput = document.getElementById("freqToMidi");
+let dBFSInput = document.getElementById("dbfsToLinear");
+let linearInput = document.getElementById("linearToDbfs");
+
+
+freqInput.addEventListener("click", freq);
+midiInput.addEventListener("click", midiPitch);
+dBFSInput.addEventListener("click", linear);
+linearInput.addEventListener("click", dBFS);
+
+console.log(midiInput)
